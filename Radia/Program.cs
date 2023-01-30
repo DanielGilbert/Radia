6c7252391f2320256e9e2fbf6be616bfa1ddc39c
Daniel Gilbert <ich@danielgilbert.de>
2023-01-30 20:09:52 +0100

    Adds X-Clacks-Overhead

--- Radia/Program.cs
+++ Radia/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Primitives;
 using Radia.Extensions.Microsoft.AspNetCore.Builders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,5 +8,16 @@ builder.AddRadiaModules();
 
 var app = builder.Build();
 app.MapRadiaModules();
+
+app.Use(async (context, next) =>
+{
+    context.Response.OnStarting(() =>
+    {
+        context.Response.Headers.Add("X-Clacks-Overhead", new StringValues(new string[] { "GNU Terry Pratchett", "GNU T. Gilbert", "GNU P. Gilbert", "GNU B. Gilbert", "GNU U. Gilbert" }));
+        context.Response.Headers.Add("X-Clacks-Overhead-Note", new StringValues(new string[] { "As long as it clacks, no one will be forgotten." }));
+        return Task.FromResult(0);
+    });
+    await next();
+});
 
 app.Run();
